b380896e14e00a44b2c7563b04905ec16b29e6cd
arpit7806 <arpitofficial7806@gmail.com>
2026-07-09 15:13:58 +0530

    making navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext.jsx";
 import "./Navbar.css";
 
 function Navbar() {
+  const { cartCount } = useCart();
+  const count = cartCount();
+
   return (
     <header className="navbar">
       <Link to="/" className="navbar-logo">
@@ -15,6 +19,11 @@ function Navbar() {
 
       <div className="navbar-cart">
         <span className="cart-icon">🛒</span>
+        {count > 0 && (
+          <span key={count} className="cart-badge">
+            {count}
+          </span>
+        )}
       </div>
     </header>
   );
